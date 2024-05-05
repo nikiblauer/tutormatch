350b6529511b888e570a6c76e1ba2c2b6b649b31
Konstantin Unterweger <e12222196@student.tuwien.ac.at>
2024-05-05 12:36:30 +0200

    feat: added Validation
    - Add regex for better matching emails

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/UserValidator.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/UserValidator.java
@@ -9,16 +9,23 @@ import java.util.List;
 
 @Component
 public class UserValidator {
+    private static final String VALIDATION_PATTERN_1 = "^[a-zA-Z0-9.+-]+@student.tuwien.ac.at";
+    private static final String VALIDATION_PATTERN_2 = "^[a-zA-Z0-9.+-]+@tuwien.ac.at";
+
+    public boolean validate(String email) throws ValidationException {
+        return email.matches(VALIDATION_PATTERN_1) || email.matches(VALIDATION_PATTERN_2);
+    }
     public void verifyUserData(ApplicationUserDto user) throws Exception {
         List<String> errors = new ArrayList<>();
-        if (!user.email.endsWith("tuwien.ac.at")) {
-            errors.add("Email has to end in tuwien.ac.at");
+        if (!validate(user.email)){
+            errors.add("Email not valid, it has to end in tuwien.ac.at or student.tuwien.ac.at");
         }
         if (user.email.length() > 255) {
             errors.add("Email is too long");
         }
         if (user.name.isEmpty()) {
             errors.add("Name cannot be null");
+            throw new ValidationException("Errors while verifying user Data:", errors);
         }
         if (user.name.equals(" ")) {
             errors.add("Name cannot be whitespace");
