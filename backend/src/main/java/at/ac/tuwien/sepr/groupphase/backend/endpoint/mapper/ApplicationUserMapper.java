f636d33d0b1c30ebdf03aba4ca5980338333a66b
Konstantin Unterweger <e12222196@student.tuwien.ac.at>
2024-05-07 16:36:31 +0200

    bug: checkstyle

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ApplicationUserMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ApplicationUserMapper.java
@@ -9,21 +9,21 @@ import org.mapstruct.Mapper;
 
 public interface ApplicationUserMapper {
     default ApplicationUserDto mapUserToDto(ApplicationUser applicationUser, ContactDetails details) {
-        if ( applicationUser == null && details == null ) {
+        if (applicationUser == null && details == null) {
             return null;
         }
 
         ApplicationUserDto applicationUserDto = new ApplicationUserDto();
 
-        if ( applicationUser != null ) {
-            applicationUserDto.setPassword( applicationUser.getPassword() );
-            applicationUserDto.setFirstname( applicationUser.getFirstname() );
-            applicationUserDto.setLastname( applicationUser.getLastname() );
-            applicationUserDto.setMatrNumber( applicationUser.getMatrNumber() );
+        if (applicationUser != null) {
+            applicationUserDto.setPassword(applicationUser.getPassword());
+            applicationUserDto.setFirstname(applicationUser.getFirstname());
+            applicationUserDto.setLastname(applicationUser.getLastname());
+            applicationUserDto.setMatrNumber(applicationUser.getMatrNumber());
         }
-        if ( details != null ) {
-            applicationUserDto.setEmail( details.getEmail() );
-            applicationUserDto.setTelNr( details.getTelNr() );
+        if (details != null) {
+            applicationUserDto.setEmail(details.getEmail());
+            applicationUserDto.setTelNr(details.getTelNr());
         }
 
         return applicationUserDto;
