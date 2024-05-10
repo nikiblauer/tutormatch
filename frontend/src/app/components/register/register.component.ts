ba5416b52ccb2e2d0c367d6d0522fa24e60c5140
Niklas Gruener <e12217059@student.tuwien.ac.at>
2024-05-10 15:18:32 +0200

    feat(frontend): added ngModel to all input fields in register component

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  password: string = '';
 
+  onSubmit() {
+    console.log(this.firstName);
+    console.log(this.lastName);
+    console.log(this.email);
+    console.log(this.password);
+  }
 }
