4f7492b945f089553ced69dbff2116af301339b4
Niklas Gruener <e12217059@student.tuwien.ac.at>
2024-05-10 14:04:56 +0200

    feat(frontend): created register component in frontend - only created html code so far for this component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
 import {AuthGuard} from './guards/auth.guard';
 import {MessageComponent} from './components/message/message.component';
+import {RegisterComponent} from "./components/register/register.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'message', canActivate: mapToCanActivate([AuthGuard]), component: MessageComponent}
 ];
 
