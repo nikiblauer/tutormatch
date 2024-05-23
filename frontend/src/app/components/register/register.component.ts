570422611c37c2fc9aa9d120202b172886391e3f
Niklas Gruener <e12217059@student.tuwien.ac.at>
2024-05-23 19:42:36 +0200

    -fix(links): fixed links in Login and Register Page to use Routerlinker instead of href

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators} from "@angular/forms";
 import {UserService} from "../../services/user.service";
 import {CreateApplicationUserDto} from "../../dtos/user";
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 import {NgIf} from "@angular/common";
 
 @Component({
@@ -11,7 +11,8 @@ import {NgIf} from "@angular/common";
   imports: [
     FormsModule,
     NgIf,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
