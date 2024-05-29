9469fad61dbc26cb5e629e486ad729e2db66a38f
Konstantin Unterweger <e12222196@student.tuwien.ac.at>
2024-05-29 23:19:10 +0200

    feat: #57 add DataGen for ratings

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RatingRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RatingRepository.java
@@ -4,9 +4,11 @@ import at.ac.tuwien.sepr.groupphase.backend.entity.UserRating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RatingRepository extends JpaRepository<UserRating, Long> {
 
     @Query("SELECT rating from UserRating where rated = :ratedStudent")
