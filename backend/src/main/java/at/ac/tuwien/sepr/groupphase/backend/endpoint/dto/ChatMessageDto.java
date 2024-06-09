04f08e7e8bc60a6a61d7b6e0c0109429f81887e5
12217059 <e12217059@tuwien.ac.at>
2024-06-09 17:34:24 +0200

    fix(mr issues)

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/ChatMessageDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/ChatMessageDto.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,9 +16,14 @@ import java.util.Date;
 @Builder
 public class ChatMessageDto {
     private Long id;
+    @NotNull
     private String chatRoomId;
+    @NotNull
     private Long senderId;
+    @NotNull
     private Long recipientId;
+    @NotNull
     private String content;
+    @NotNull
     private Date timestamp;
 }
