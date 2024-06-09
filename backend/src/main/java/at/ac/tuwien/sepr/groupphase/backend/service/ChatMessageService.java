d4d953f4d9ac6dda1f5b241f43889bd221ce6573
12217059 <e12217059@tuwien.ac.at>
2024-06-09 16:45:19 +0200

    fix(checkstyle)

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/ChatMessageService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/ChatMessageService.java
@@ -6,14 +6,16 @@ import java.util.List;
 
 public interface ChatMessageService {
     /**
-     * Retrieving all chat messages for the specified chatroom id
+     * Retrieving all chat messages for the specified chatroom id.
+     *
      * @param chatRoomId chatroom to get the messages from
      * @return list of all chat messages in this chatroom
      */
     List<ChatMessageDto> getChatMessagesByChatRoomId(String chatRoomId);
 
     /**
-     * This function persists the specified chat messages in the persistent data store
+     * This function persists the specified chat messages in the persistent data store.
+     *
      * @param chatMessageDto message to persist
      */
     void saveChatMessage(ChatMessageDto chatMessageDto);
