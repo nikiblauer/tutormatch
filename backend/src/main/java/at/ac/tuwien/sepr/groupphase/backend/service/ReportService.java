f662a6d4e83b3a412bbf61e3f25c8099942bebac
Konstantin Unterweger <e12222196@student.tuwien.ac.at>
2024-06-17 19:17:33 +0200

    fix: #102 mr issues

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/ReportService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/ReportService.java
@@ -9,13 +9,48 @@ import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
 import java.util.List;
 
 public interface ReportService {
+    /**
+     * this methode is used to report a user by another user.
+     *
+     * @param reporter     the user that reports
+     * @param reportedUser the user that is being reported
+     * @param reason       the reason for reporting the user reportedUser
+     * @throws ValidationException an exception if the reason is too long for the SQL database
+     */
     void reportUser(ApplicationUser reporter, ApplicationUser reportedUser, String reason) throws ValidationException;
 
+    /**
+     * this methode is used to report a user by another user and the feedback from that user.
+     *
+     * @param reporter     the user that reports
+     * @param reportedUser the user that is being reported
+     * @param reason       the reason for reporting the user reportedUser
+     * @param feedback     the feedback that was being reported
+     * @throws ValidationException an exception if the reason is too long for the SQL database
+     */
     void reportUserFeedback(ApplicationUser reporter, ApplicationUser reportedUser, String reason, Feedback feedback) throws ValidationException;
 
+    /**
+     * this methode is used to report a user by another user and the chat from that user with the reporter chat.
+     *
+     * @param reporter the user that reports
+     * @param chatId   the chat that is being reported
+     * @param reason   the reason for reporting the user reportedUser
+     * @throws ValidationException an exception if the reason is too long for the SQL database
+     */
     void reportUserChat(String chatId, ApplicationUser reporter, String reason) throws ValidationException;
 
+    /**
+     * gets all reports from the database.
+     *
+     * @return a list with all reports stored in the database
+     */
     List<Report> getAllReports();
 
+    /**
+     * deletes a report with the corresponding id.
+     *
+     * @param id the id of the report that is going to be deleted
+     */
     void deleteReport(Long id);
 }
