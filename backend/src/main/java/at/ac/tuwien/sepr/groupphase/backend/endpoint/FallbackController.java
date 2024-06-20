e0767ed2517cfcf8b7222f2603a270da4f296dc4
Elias Moser <e12116436@student.tuwien.ac.at>
2024-06-20 18:55:26 +0200

    fix: attempt to fix 404 not found with new FallbackController.java and new copy command in pipeline

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/FallbackController.java
@@ -0,0 +1,26 @@
+package at.ac.tuwien.sepr.groupphase.backend.endpoint;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+public class FallbackController {
+    @RequestMapping(value = "/**/{path:[^\\.]*}", method = RequestMethod.GET)
+    @ResponseBody
+    public String redirect(HttpServletRequest request) throws IOException {
+        Resource resource = new ClassPathResource("/frontend/dist/sepr-group-phase/browser/index.html");
+        Path path = Paths.get(resource.getURI());
+        String content = new String(Files.readAllBytes(path), "UTF-8");
+        return content;
+    }
+}
