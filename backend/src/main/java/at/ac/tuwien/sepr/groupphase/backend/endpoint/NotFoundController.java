f019b19726111133c9adebfe58a5d6c89b648e31
Elias Moser <e12116436@student.tuwien.ac.at>
2024-06-21 07:46:33 +0200

    fix: failing test

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/NotFoundController.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/NotFoundController.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 
+import jakarta.annotation.security.PermitAll;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotFoundController implements ErrorController {
 
     @RequestMapping("/error")
+    @PermitAll
     public String handleError() {
         return "forward:/notFound";
     }
