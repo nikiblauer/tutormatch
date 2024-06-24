6efc054156f77a3b24133523a59b4ae51e0a3c47
Konstantin Unterweger <e12222196@student.tuwien.ac.at>
2024-06-24 16:08:39 +0200

    fix: #120 backend validation size now checked no 500 errors due to size

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/UpdateStudentDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/UpdateStudentDto.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,17 +12,21 @@ public class UpdateStudentDto {
     public Long id;
 
     @NotBlank(message = "Firstname is mandatory")
+    @Size(max = 255)
     public String firstname;
 
     @NotBlank(message = "Lastname is mandatory")
+    @Size(max = 255)
     public String lastname;
 
     public String telNr;
 
+    @Size(max = 255)
     public String street;
 
     public Integer areaCode;
 
+    @Size(max = 255)
     public String city;
 
     @Override
